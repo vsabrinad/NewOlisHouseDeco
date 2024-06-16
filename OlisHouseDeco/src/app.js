c7114da396e21572eca747615b2d1775f3f5dca0
Sabrina <sabrinadvillarreal@hotmail.com>
2024-06-15 22:27:47 -0300

    intento 1

--- OlisHouseDeco/src/app.js
+++ OlisHouseDeco/src/app.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import productsRouter from './routes/products.router.js';
-import petsRouter from './routes/pets.router.js';
 import carritoRouter from '.routes/carrito.router.js';
-import customMiddleware from './middlewares/customMiddleware.js';
 
 const app = express();
 
@@ -12,10 +10,6 @@ app.listen(PORT,()=>console.log(`Listening on PORT ${PORT}`));
 
 app.use(express.json());
 
-app.use(express.static('./src/public'))
-
-//app.use(customMiddleware);
 
 app.use('/api/products',productsRouter);
-app.use('/api/pets',petsRouter);
 app.use('/api/carrito',carritoRouter);
